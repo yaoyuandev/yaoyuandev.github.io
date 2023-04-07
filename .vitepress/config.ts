f891d7b6959008b6608dc76121157bce8c5e52ca
Yao Yuan <yaoyuanyy@outlook.com>
2023-04-07 23:07:55 +0800

    rename blog

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "遥远之境",
-  titleTemplate: '姚远的个人空间',
+  titleTemplate: '探索心灵',
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      {text: '博客', link: 'https://www.yuque.com/yaoyuanyy/blog'},
+      {text: '文集', link: 'https://www.yuque.com/yaoyuanyy/blog'},
       {text: '软件', link: 'software/'},
       {text: '教程', link: 'guide/'},
     ],
--- index.md
+++ index.md
@@ -6,7 +6,7 @@ hero:
   name: "欢迎访问"
   tagline: "你好！我是姚远，一名软件工程师，现居上海。\n目前在『百度』工作，之前在『第四范式』，再之前是『ACMer』。\n我的兴趣是效率软件。\n最近在玩 ChatGPT......"
 features:
-- title: "博客"
+- title: "文集"
   details: "随便写写"
   link: 'https://www.yuque.com/yaoyuanyy/blog'
 - title: "软件"
